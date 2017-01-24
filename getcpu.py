dbe4e4984660d6da89ac0bee374e4c7e49ae2b93
Sander Jonkers <sander.jonkers+github@gmail.com>
2017-01-24 18:38:43 +0100

    Remove unneeded spaces in cputype

--- getcpu.py
+++ getcpu.py
@@ -4,33 +4,37 @@ import platform, subprocess
 def getcpu():
     # find the CPU name (which needs a different method per OS), and return it
     # return None if none found
+    # works on Linux, MacOS (aka OSX), Windows
+
+    cputype = None
 
     try:
         if platform.system() == "Windows":
-            import _winreg as winreg	# Python 2
+            import _winreg as winreg	# needed on Python 2
             key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"Hardware\Description\System\CentralProcessor\0")
-            processor_brand = winreg.QueryValueEx(key, "ProcessorNameString")[0]
+            cputype = winreg.QueryValueEx(key, "ProcessorNameString")[0]
             winreg.CloseKey(key)
-            return processor_brand
 
         elif platform.system() == "Darwin":
-            return subprocess.check_output(['sysctl', "-n", "machdep.cpu.brand_string"]).strip()
+            cputype = subprocess.check_output(['sysctl', "-n", "machdep.cpu.brand_string"]).strip()
 
         elif platform.system() == "Linux":
             for myline in open("/proc/cpuinfo"):
                 if myline.startswith(('model name')):
                     # Typical line:
                     # model name      : Intel(R) Xeon(R) CPU           E5335  @ 2.00GHz
-                    return myline.split(":", 1)[1]	# get everything after the first ":"
+                    cputype = myline.split(":", 1)[1]	# get everything after the first ":"
+                    break # we're done
 
     except:
-        # An exception could happen due to a subprocess call
+        # An exception, maybe due to a subprocess call gone wrong
+        cputype = "Exception happened"
         pass
 
-    # we couldn't determine the processor name, due to
-    # an exception, or
-    # not Windows, nor Darwin, nor Linux, so possibly BSD, SUN, ...
-    return None
+    if cputype:
+        # remove unnneeded space:
+        cputype = " ".join(cputype.split())
+    return cputype
 
 
 
